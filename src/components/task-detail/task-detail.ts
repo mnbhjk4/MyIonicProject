ad37cc64421c9c6c828690b4410ddb90253a9758
Agi Wu <agi_wu@raytrex.com>
2017-06-08 18:06:11 +0800

    新增Planner detail 內容

--- src/components/task-detail/task-detail.ts
+++ src/components/task-detail/task-detail.ts
@@ -14,9 +14,27 @@ import { NavParams } from 'ionic-angular';
 export class TaskDetailComponent {
 
   task_id : string;
+  start_date : string = "04/04/2017";
+  end_date : string = "04/04/2017";
 
+  itemArray:Array<{id:string,checked:boolean,itemContent:string}> = [
+    {id:"1",checked:true,itemContent:"Design Document"},
+    {id:"2",checked:false,itemContent:"Coding"},
+    {id:"3",checked:false,itemContent:"Debug"},
+    {id:"4",checked:false,itemContent:"Release"}
+  ];
   constructor(private navParams: NavParams) {
     this.task_id = this.navParams.get("task_id");
   }
 
+  addRow(){
+    this.itemArray.push({id:"asd",checked:false,itemContent:""})
+  }
+  removeRow(id:string){
+    for(let i = 0;i < this.itemArray.length;i++){
+      if(this.itemArray[i].id == id){
+        this.itemArray.splice(i,1);
+      }
+    }
+  }
 }
