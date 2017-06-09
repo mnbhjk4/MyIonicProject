b27d5e30ba3444f9144c81406c92b42544f9b778
Agi Wu <agi_wu@raytrex.com>
2017-06-09 10:06:34 +0800

    Task detail content udpate

--- src/components/task-detail/task-detail.ts
+++ src/components/task-detail/task-detail.ts
@@ -14,14 +14,22 @@ import { NavParams } from 'ionic-angular';
 export class TaskDetailComponent {
 
   task_id : string;
-  start_date : string = "04/04/2017";
-  end_date : string = "04/04/2017";
+  start_date : string = new Date().toISOString();
+  end_date : string =  new Date().toISOString();
 
   itemArray:Array<{id:string,checked:boolean,itemContent:string}> = [
     {id:"1",checked:true,itemContent:"Design Document"},
     {id:"2",checked:false,itemContent:"Coding"},
     {id:"3",checked:false,itemContent:"Debug"},
     {id:"4",checked:false,itemContent:"Release"}
+  ];
+  commentArray:Array<Comment>=
+  [
+    new Comment("E","Agi wu","eee",new Date(),[{title:"Yahoo",url:"http://tw.yahoo.com"},{title:"Raytrex",url:"http://www.raytrex.com"}]),
+    new Comment("D","Agi wu","ccc",new Date(),[{title:"Yahoo",url:"http://tw.yahoo.com"},{title:"Raytrex",url:"http://www.raytrex.com"}]),
+    new Comment("C","Agi wu","Test oajea",new Date(),[{title:"Yahoo",url:"http://tw.yahoo.com"},{title:"Raytrex",url:"http://www.raytrex.com"}]),
+    new Comment("B","Agi wu","开始测试中文简体是否正常显示",new Date(),[{title:"Yahoo",url:"http://tw.yahoo.com"},{title:"Raytrex",url:"http://www.raytrex.com"}]),
+    new Comment("A","Agi wu","開始測試中文是否正常顯示",new Date(),[{title:"Yahoo",url:"http://tw.yahoo.com"},{title:"Raytrex",url:"http://www.raytrex.com"}]),
   ];
 
   description : string = "description testing";
@@ -38,5 +46,20 @@ export class TaskDetailComponent {
         this.itemArray.splice(i,1);
       }
     }
+  }
+}
+
+export class Comment{
+  comment_id:string;
+  editor:string;
+  comment:string;
+  comment_date:Date;
+  attachs:Array<{title:string,url:string}>;
+  constructor(comment_id:string,editor:string,comment:string,comment_date:Date,attachs:Array<{title:string,url:string}>){
+    this.comment_id = comment_id;
+    this.comment = comment;
+    this.comment_date = comment_date;
+    this.attachs = attachs;
+    this.editor = editor;
   }
 }
