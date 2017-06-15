0f297c0a85e96c48800a22f05344c5559c12833a
Agi Wu <agi_wu@raytrex.com>
2017-06-15 16:33:42 +0800

    Sync only

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,6 +1,6 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Http,RequestOptions,Headers} from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { MenuController, Nav } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 
@@ -23,42 +23,43 @@ import { PlannerPage } from '../planner/planner';
   templateUrl: 'index.html',
 })
 export class IndexPage {
-   @ViewChild(Nav) nav: Nav;
-  pages: Array<{ title: string,icon:string ,component: any }>;
-  rootPage : any = ListPage;
-  private user_name : string = "";
-  constructor(public navCtrl: NavController, 
-  public navParams: NavParams,
-  private http : Http, 
-  public menu: MenuController,
-  public loginProvider : LoginProvider,
-  private storage : Storage) {
-     // set our app's pages
+  @ViewChild(Nav) nav: Nav;
+  pages: Array<{ title: string, icon: string, component: any }>;
+  rootPage: any = ListPage;
+  private user_name: string = "";
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private http: Http,
+    public menu: MenuController,
+    public loginProvider: LoginProvider,
+    private storage: Storage) {
+    // set our app's pages
     this.pages = [
-      { title: 'Index', icon: 'ios-home-outline' ,component: ListPage },
-      { title: 'Project', icon: 'ios-boat-outline' ,component: ProjectPage },
-      { title:'Planner', icon: 'ios-link-outline' ,component:PlannerPage},
-      { title: 'Leave system',icon: 'ios-bicycle-outline' ,component:LeavePage},
-      { title: 'Organize' , icon: 'ios-people-outline' ,component: OrganizePage}
+      { title: 'Index', icon: 'ios-home-outline', component: ListPage },
+      { title: 'Project', icon: 'ios-boat-outline', component: ProjectPage },
+      { title: 'Planner', icon: 'ios-link-outline', component: PlannerPage },
+      { title: 'Leave system', icon: 'ios-bicycle-outline', component: LeavePage },
+      { title: 'Organize', icon: 'ios-people-outline', component: OrganizePage }
     ];
   }
 
   ionViewDidLoad() {
-   this.storage.get("access_obj").then((access_obj)=>{
-    let id_token_string = access_obj.decoded_access_token;
-    if(id_token_string != null && id_token_string != ""){
-      let id_token = JSON.parse(id_token_string);
-      if(id_token.name != null && id_token.name != ""){
-        this.user_name = id_token.name;
-        this.loginProvider.getUserProfilePhoto(access_obj.access_token).subscribe(
-          result=>{
-            console.log(result["@odata.id"]);
-          }
-        );
+    this.storage.get("access_obj").then((access_obj) => {
+      let id_token_string = access_obj.decoded_access_token;
+      if (id_token_string != null && id_token_string != "") {
+        let id_token = JSON.parse(id_token_string);
+        if (id_token.name != null && id_token.name != "") {
+          this.user_name = id_token.name;
+          this.loginProvider.getUserProfilePhoto(access_obj.access_token).subscribe(
+            result => {
+              console.log(result["@odata.id"]);
+            }
+          );
+
+        }
       }
-    }
-   });
-  
+    });
+
   }
 
   openPage(page) {
@@ -68,7 +69,7 @@ export class IndexPage {
     this.nav.setRoot(page.component);
   }
 
-  logout(){
+  logout() {
     this.loginProvider.logout();
   }
 }
